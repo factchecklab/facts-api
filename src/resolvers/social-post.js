1f18bd3b05e5a2b351ecf8d080368590a7a9852e
Kwok-kuen Cheung <me@cheungpat.com>
2020-04-23 20:43:16 +0800

    weather: Implement searching social post

--- src/resolvers/social-post.js
+++ src/resolvers/social-post.js
@@ -1,15 +1,109 @@
+const stringifyCursor = (cursor) => {
+  return Buffer.from(JSON.stringify(cursor)).toString('base64');
+};
+
+const parsePagingCursor = (cursorString) => {
+  try {
+    const parsed = JSON.parse(
+      Buffer.from(cursorString, 'base64').toString('ascii')
+    );
+    return Array.isArray(parsed) ? parsed : undefined;
+  } catch (e) {
+    return undefined;
+  }
+};
+
+const esSortObject = (orderBy, reverse) => {
+  // Default sort field, also used for sorting if the primary sort field
+  // is the same (post published at the same time).
+  const sort = [
+    {
+      _id: reverse ? 'desc' : 'asc',
+    },
+  ];
+
+  // TODO(cheungpat): Implement other sorting. The following code sort by newest.
+  sort.unshift({
+    time: {
+      order: reverse ? 'asc' : 'desc',
+    },
+  });
+  return sort;
+};
+
+const esQueryObject = (keywords) => {
+  return keywords
+    ? {
+        /* eslint-disable-next-line camelcase */
+        multi_match: {
+          query: keywords,
+          fields: ['title', 'content'],
+        },
+      }
+    : undefined;
+};
+
 export default {
   Query: {
-    socialPosts: () => {
-      return {
-        nodes: [
-          {
-            postedAt: new Date(),
+    socialPosts: async (parent, args, { elastic }) => {
+      const forward = !!args.after || !args.before;
+      const afterCursor = args.after || args.before || undefined;
+      const reverse = !forward || args.reverse;
+      const size = args.first || 10;
+
+      const { body } = await elastic.search({
+        index: 'threads',
+        size: size + 1, // +1 to find out if more data is available
+        body: {
+          query: esQueryObject(args.keywords),
+          sort: esSortObject(args.orderBy, reverse),
+          /* eslint-disable-next-line camelcase */
+          search_after: afterCursor
+            ? parsePagingCursor(afterCursor)
+            : undefined,
+        },
+      });
+
+      const result = body.hits.hits;
+      const hasMore = result.length > size;
+      result.splice(size);
+      if (!forward) {
+        result.reverse();
+      }
+
+      const edges = result.map(({ _source: doc, sort }) => {
+        return {
+          cursor: stringifyCursor(sort),
+          node: {
+            postedAt: new Date(doc.time),
+            title: doc.title,
+            content: doc.content,
             platform: {
               name: 'lihkg',
             },
+            poster: {
+              name: doc.author,
+            },
           },
-        ],
+        };
+      });
+
+      return {
+        edges,
+        nodes: edges.map((edge) => {
+          return edge.node;
+        }),
+        pageInfo: {
+          startCursor:
+            result.length > 0 ? stringifyCursor(result[0].sort) : null,
+          endCursor:
+            result.length > 0
+              ? stringifyCursor(result[result.length - 1].sort)
+              : null,
+          hasNextPage: forward && hasMore,
+          hasPreviousPage: !forward && hasMore,
+        },
+        totalCount: body.hits.total.value,
       };
     },
   },
@@ -20,27 +114,6 @@ export default {
       return {
         name: 'lihkg',
       };
-    },
-  },
-  SocialPostConnection: {
-    edges: (parent) => {
-      return parent.nodes.map((node) => {
-        return {
-          cursor: '42',
-          node: node,
-        };
-      });
-    },
-    pageInfo: (parent, args, context, info) => {
-      return {
-        startCursor: '42',
-        endCursor: '42',
-        hasNextPage: false,
-        hasPreviousPage: false,
-      };
-    },
-    totalCount: () => {
-      return 42;
     },
   },
 };
