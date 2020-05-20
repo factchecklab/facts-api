375536d110605d2941b2702f2c19aab513d7efc1
Kwok-kuen Cheung <me@cheungpat.com>
2020-05-21 03:31:14 +0800

    Move asset upload code into util function

--- src/resolvers/asset.js
+++ src/resolvers/asset.js
@@ -1,56 +1,11 @@
-import mime from 'mime-types';
-import { v4 as uuidv4 } from 'uuid';
-import path from 'path';
-import { generate as generateAssetToken } from '../util/asset-token';
-
-export const generateAssetUrl = async (asset, options, { storage }) => {
-  const bucket = storage.bucket(process.env.ASSETS_STORAGE_BUCKET);
-  const file = bucket.file(asset.path);
-
-  // Generate a presigned URL with an expiry of 2 hours, starting from the
-  // beginning of the current hour.
-  // This generates a presigned URL that doesn't change for at most 1 hour.
-  const hourInMilliseconds = 60 * 60 * 1000;
-  const expires =
-    Math.floor(new Date().getTime() / hourInMilliseconds + 2) *
-    hourInMilliseconds;
-  const result = await file.getSignedUrl({
-    action: 'read',
-    expires,
-    ...(options || {}),
-  });
-  return result.length > 0 ? result[0] : 1;
-};
+import {
+  generateAssetUrl,
+  generateAssetToken,
+  uploadAsset,
+} from '../util/asset';
 
 export const uploadAndBuildAsset = async (file, { models, storage }) => {
-  const { filename, mimetype, createReadStream } = await file;
-
-  const bucket = storage.bucket(process.env.ASSETS_STORAGE_BUCKET);
-  const filepath = path.posix.join(
-    process.env.ASSETS_STORAGE_PREFIX,
-    `${uuidv4()}.${mime.extension(mimetype)}`
-  );
-  const gcsfile = bucket.file(filepath);
-
-  const stream = gcsfile.createWriteStream({
-    contentType: mimetype,
-  });
-
-  await new Promise((resolve, reject) => {
-    createReadStream()
-      .pipe(stream)
-      .on('error', (err) => {
-        reject(err);
-      })
-      .on('finish', () => {
-        resolve();
-      });
-  });
-  return models.Asset.build({
-    contentType: mimetype,
-    path: filepath,
-    filename: filename,
-  });
+  return models.Asset.build(await uploadAsset(storage, file));
 };
 
 export default {
@@ -62,20 +17,20 @@ export default {
   },
 
   Asset: {
-    url: (asset, args, context) => {
-      return generateAssetUrl(asset, {}, context);
+    url: (asset, args, { storage }) => {
+      return generateAssetUrl(storage, asset, {});
     },
-    downloadUrl: (asset, args, context) => {
-      return generateAssetUrl(asset, { promptSaveAs: asset.filename }, context);
+    downloadUrl: (asset, args, { storage }) => {
+      return generateAssetUrl(storage, asset, { promptSaveAs: asset.filename });
     },
   },
 
   UploadedAsset: {
-    url: (asset, args, context) => {
-      return generateAssetUrl(asset, {}, context);
+    url: (asset, args, { storage }) => {
+      return generateAssetUrl(storage, asset, {});
     },
-    downloadUrl: (asset, args, context) => {
-      return generateAssetUrl(asset, { promptSaveAs: asset.filename }, context);
+    downloadUrl: (asset, args, { storage }) => {
+      return generateAssetUrl(storage, asset, { promptSaveAs: asset.filename });
     },
     token: (asset, args, context) => {
       return generateAssetToken(asset.id);
