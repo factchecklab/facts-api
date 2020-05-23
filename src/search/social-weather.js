cf9ddc2e9e525128c4dec3f0449c440a8ed8fcec
Kwok-kuen Cheung <me@cheungpat.com>
2020-05-23 18:45:00 +0800

    Implement full search query parser
    The following are support in addition to previously supported syntax:
    
    * quoted phrase search "Hello World"
    * range search `interactions`, `replies`, `likes`, `dislikes`
    * boolean groupings
    * `id` specific condition like `groupid`, `userid` etc

--- src/search/social-weather.js
+++ src/search/social-weather.js
@@ -1,4 +1,11 @@
-import searchQuery from 'search-query-parser';
+import {
+  parse,
+  terms,
+  multimatch,
+  match,
+  prefixterms,
+  range,
+} from '../util/search-query-parser';
 
 // `from` and `interval` are used by elasticsearch, while
 // `unit` is used by postgres
@@ -39,169 +46,35 @@ export const reactionDeltaModels = {
 
 export const defaultTimeframe = '1m';
 
+const builder = {
+  text: (values) => multimatch(['title', 'content'], values),
+  title: (values) => match('title', values),
+  content: (values) => match('conent', values),
+  group: (values) => terms('group.name.keyword', values),
+  groupid: (values) =>
+    prefixterms('group.id.keyword', 'group.platform_name.keyword', values),
+  user: (values) => terms('user.name.keyword', values),
+  userid: (values) =>
+    prefixterms('user.id.keyword', 'user.platform_name.keyword', values),
+  url: (values) => terms('content_urls.keyword', values),
+  postid: (values) => prefixterms('_id', 'platform_name.keyword', values),
+  platform: (values) => terms('platform_name.keyword', values),
+  interactions: (values) => range('interaction_count', values),
+  replies: (values) => range('reply_count', values),
+  likes: (values) => range('like_count', values),
+  dislikes: (values) => range('dislike_count', values),
+};
+
 export const esQueryObject = (query, timeframe) => {
-  // Given a key and an array, convert it into an elasticsearch query object conjuncted with and's
-  const parsedQuery = searchQuery.parse(query, {
-    keywords: [
-      'title',
-      'content',
-      'platform',
-      'group',
-      'user',
-      'url',
-      'post',
-      'text',
-    ],
-    alwaysArray: true,
-    tokenize: true,
-  });
+  const boolQuery = parse(query, builder);
 
   // Sets the time range
   const filterClause = {
     // eslint-disable-next-line camelcase
     range: { created_at: { from: timeframe.from } },
   };
-  const mustClauses = buildBooleanClauses(parsedQuery);
-  const mustNotClauses = buildBooleanClauses(parsedQuery.exclude);
 
   // Update query object
-  const boolQuery = {};
   boolQuery.filter = filterClause;
-  if (mustClauses.length) {
-    boolQuery.must = mustClauses;
-  }
-  if (mustNotClauses.length) {
-    // eslint-disable-next-line camelcase
-    boolQuery.must_not = mustNotClauses;
-  }
-
-  return {
-    bool: boolQuery,
-  };
-};
-
-/**
- * Return query clauses for elasticsearch boolean query.
- *
- * Returns an array of clauses for specifying a boolean query 'occurrence'
- * such as 'must', 'must_not'.
- *
- */
-export const buildBooleanClauses = (conditions) => {
-  let { text } = conditions;
-  const { title, content, group, user, url, post, platform } = conditions;
-  const clauses = [];
-
-  /**
-   * This section handles text, title, and content, which use 'match' to
-   * search for textual data.
-   */
-
-  // Sets the title condition
-  if (text) {
-    // for some reason `alwaysArray` does not take effect in exclude list, hence
-    // wrapping the text value as array if it is not an array.
-    if (!Array.isArray(text)) {
-      text = [text];
-    }
-    if (text.length > 0) {
-      const subClauses = text.map((value) => {
-        return {
-          // eslint-disable-next-line camelcase
-          multi_match: {
-            query: value,
-            fields: ['text', 'content'],
-          },
-        };
-      });
-      clauses.push(
-        subClauses.length === 1
-          ? subClauses[0]
-          : { bool: { should: subClauses } }
-      );
-    }
-  }
-
-  // Sets the title condition
-  if (title && title.length > 0) {
-    const subClauses = title.map((value) => {
-      return { match: { title: value } };
-    });
-    clauses.push(
-      subClauses.length === 1 ? subClauses[0] : { bool: { should: subClauses } }
-    );
-  }
-
-  // Sets the content condition
-  if (content && content.length > 0) {
-    const subClauses = content.map((value) => {
-      return { match: { content: value } };
-    });
-    clauses.push(
-      subClauses.length === 1 ? subClauses[0] : { bool: { should: subClauses } }
-    );
-  }
-
-  /**
-   * This section handles other fields that match by 'term' (exact match).
-   */
-
-  // Sets the group condition
-  if (group && group.length > 0) {
-    const platformList = group
-      .filter((value) => {
-        return value.endsWith('_*');
-      })
-      .map((value) => {
-        return value.replace(/_\*$/, '');
-      });
-    const groupList = group.filter((value) => {
-      return !value.endsWith('_*');
-    });
-
-    const subClauses = [];
-    if (platformList.length > 0) {
-      subClauses.push({
-        terms: { 'group.platform_name.keyword': platformList },
-      });
-    }
-    if (groupList.length > 0) {
-      subClauses.push({
-        terms: { 'group.id.keyword': groupList },
-      });
-    }
-
-    clauses.push(
-      subClauses.length === 1 ? subClauses[0] : { bool: { should: subClauses } }
-    );
-  }
-
-  // Sets the group condition
-  if (user && user.length > 0) {
-    clauses.push({
-      bool: {
-        should: [
-          { terms: { 'user.id.keyword': user } },
-          { terms: { 'user.name.keyword': user } },
-        ],
-      },
-    });
-  }
-
-  // Sets the URL condition
-  if (url && url.length > 0) {
-    clauses.push({ terms: { 'content_urls.keyword': url } });
-  }
-
-  // Sets the post condition
-  if (post && post.length > 0) {
-    clauses.push({ terms: { _id: post } });
-  }
-
-  // Sets the platform condition
-  if (platform && platform.length > 0) {
-    clauses.push({ terms: { 'platform_name.keyword': platform } });
-  }
-
-  return clauses;
+  return { bool: boolQuery };
 };
---
+++ src/util/search-query-parser/helper.js
@@ -0,0 +1,109 @@
+const isQuotedString = (str) => {
+  return str.startsWith('"') || str.endsWith("'");
+};
+
+const unquoteString = (str) => {
+  if (!isQuotedString(str)) {
+    return str;
+  }
+
+  return str.substring(1, str.length - 1).replace(/\\/g, '');
+};
+
+export const terms = (fieldname, values) => {
+  if (!Array.isArray(values) || !values.length) {
+    return null;
+  }
+  return { terms: { [fieldname]: values.map(unquoteString) } };
+};
+
+export const prefixterms = (exactField, prefixField, values) => {
+  if (!Array.isArray(values)) {
+    return null;
+  }
+
+  const [exactValues, prefixValues] = values.reduce(
+    (result, v) => {
+      const value = unquoteString(v);
+      if (value.endsWith('_*')) {
+        result[1].push(value.replace(/_\*$/, ''));
+      } else {
+        result[0].push(value);
+      }
+      return result;
+    },
+    [[], []]
+  );
+
+  const subClauses = [
+    terms(prefixField, prefixValues),
+    terms(exactField, exactValues),
+  ].filter((v) => v);
+
+  if (subClauses.length === 0) {
+    return null;
+  } else if (subClauses.length === 1) {
+    return subClauses[0];
+  } else {
+    return { bool: { should: subClauses } };
+  }
+};
+
+export const match = (fieldname, values) => {
+  if (!Array.isArray(values) || !values.length) {
+    return null;
+  }
+
+  const subClauses = values.map((value) => {
+    if (isQuotedString(value)) {
+      // eslint-disable-next-line camelcase
+      return { match_phrase: { [fieldname]: unquoteString(value) } };
+    } else {
+      return { match: { [fieldname]: value } };
+    }
+  });
+  return subClauses.length === 1
+    ? subClauses[0]
+    : { bool: { should: subClauses } };
+};
+
+export const multimatch = (fieldnames, values) => {
+  if (!Array.isArray(values) || !values.length) {
+    return null;
+  }
+
+  const subClauses = values.map((value) => {
+    return {
+      // eslint-disable-next-line camelcase
+      multi_match: {
+        query: unquoteString(value),
+        type: isQuotedString(value) ? 'phrase' : 'best_fields',
+        fields: fieldnames,
+      },
+    };
+  });
+  return subClauses.length === 1
+    ? subClauses[0]
+    : { bool: { should: subClauses } };
+};
+
+export const range = (fieldname, values) => {
+  if (!Array.isArray(values) || values.length !== 2) {
+    return null;
+  }
+
+  const [from, to] = values;
+  if (!from && !to) {
+    return null;
+  }
+
+  const rangeObj = {};
+  if (from) {
+    rangeObj.from = from;
+  }
+  if (to) {
+    rangeObj.to = to;
+  }
+
+  return { range: { [fieldname]: rangeObj } };
+};
